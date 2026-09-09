27458c13a2652af39305a91055abe1c003d4d679
EmanueleMoraesCampos <campos.emanuele@escola.pr.gov.br>
2026-09-09 11:45:24 -0300

    Create Board.java

---
+++ src/Board.java
@@ -0,0 +1,17 @@
+import  java.awt.*;
+
+public class Board {
+    
+    public  void draw(Graphics2D g2) {
+        drawGrass(g2);
+    }
+    
+    private  void drawGrass(Graphics2D g2) {
+        int tile = GameConstants.TILE;
+        
+        for (int x = 0; x < GameConstants.COLS; x++) {
+            
+            for (int x = < Game)
+        }
+    }
+}
